d4fe33d5fad0222fec1b458364f782171de74de0
lenovo <quanquanting@qq.com>
2019-05-17 20:37:12 +0800

    1.帖子解析，保存到csv

--- AutoHomeSpiderClass.py
+++ AutoHomeSpiderClass.py
@@ -10,6 +10,7 @@
 #
 from bs4 import BeautifulSoup
 import requests
+import csv
 import re
 import random
 
@@ -31,9 +32,28 @@ class AutoHomeSpider:
         }
         self.seriesApi = "https://club.autohome.com.cn/frontapi/bbs/getSeriesByLetter?firstLetter=%s"  # 所有车系论坛链接api
         self.forumUrl = "https://club.autohome.com.cn/bbs/forum-c-%d-1.html"
+        self.replyUrl = "https://clubajax.autohome.com.cn/topic/rv"
 
     def get_html(self, url):
         res = requests.get(url, headers=self.headers, timeout=5)
+        return res
+
+    def get_reply_view(self, idList):
+        """获取某帖子的回复量和访问量"""
+        headers = {
+            "Host": "clubajax.autohome.com.cn",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
+            "Accept": "*/*",
+            "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+        }
+        idStr = ""
+        idStr = idStr + ",".join(idList)
+        params = {
+            "ids": idStr
+        }
+        res = requests.get(self.replyUrl, headers=headers, params=params)
         return res
 
     def get_bbs_url(self):
@@ -58,22 +78,57 @@ class AutoHomeSpider:
         """解析论坛帖子"""
         soup = BeautifulSoup(res.text, 'html.parser')
         postList = soup.find_all('dl', {'class': 'list_dl'})
+        topic = dict()
+        topic['url'] = list()           # 帖子链接列表
+        topic['title'] = list()          # 帖子标题列表
+        topic['date'] = list()          # 发帖日期列表
+        topic['author'] = list()        # 发帖用户列表
+        topic['id'] = list()            # 帖子Id列表
+        topic['reply'] = list()    # 帖子回复量列表
+        topic['views'] = list()     # 帖子访问量列表
         for post in postList:
             try:
-                topic = post.find('a', {'class', 'a_topic'})
-                
-                print(topic['href'])
-                print(topic.get_text())
+                dt = post.find('a', {'class', 'a_topic'})
+                topicUrl = dt['href']        # 帖子链接
+                topicTitle = dt.get_text()    # 帖子标题
+                topicDate = post.find('span', {'class', 'tdate'}).get_text()                # 发帖日期
+                topicAuthor = post.find('a', {'class', 'linkblack'}).get_text()             # 发帖用户
+                topicId = post.find('dd', {'class', 'cli_dd'})['lang']                      # 帖子ID
+                topic['url'].append(topicUrl)
+                topic['title'].append(topicTitle)
+                topic['date'].append(topicDate)
+                topic['author'].append(topicAuthor)
+                topic['id'].append(topicId)
             except Exception as e:
                 print(e)
-        # print(postList)
+        # 异步获取访问量和回复量
+        res = self.get_reply_view(topic['id'])
+        for i in range(len(topic['id'])):
+            for reply in res.json():
+                if str(reply['topicid']) == str(topic['id'][i]):
+                    topic['reply'].append(reply['replys'])
+                    topic['views'].append(reply['views'])
+        return topic
 
 
 def main():
     url = 'https://club.autohome.com.cn/bbs/forum-c-403-2.html'
     auto = AutoHomeSpider()
     res = auto.get_html(url)
-    auto.analysis_forumPost(res)
+    topic = auto.analysis_forumPost(res)
+
+    # 写入csv文件
+    headers = ['Id', 'Title', 'Author', 'Reply', 'Views', 'Date', 'Url']
+    rows = list()
+    for i in range(len(topic['id'])):
+        rows.append((
+                    topic['id'][i], topic['title'][i], topic['author'][i], topic['reply'][i], topic['views'][i],
+                    topic['date'][i], "https://club.autohome.com.cn" + topic['url'][i]))
+    with open("test.csv", "a", newline="", encoding='utf_8_sig') as fw:
+        f_csv = csv.writer(fw)
+        f_csv.writerow(headers)
+        f_csv.writerows(rows)
+        print("CSV文件保存成功！")
 
 
 if __name__ == '__main__':
