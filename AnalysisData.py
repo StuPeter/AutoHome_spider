53b0ed2549e0f3a1de662ee627141ca405615359
QQT <quanquanting@qq.com>
2019-07-09 10:16:00 +0800

    1.直方图绘制

--- AnalysisData.py
+++ AnalysisData.py
@@ -10,14 +10,14 @@
 #
 from pyecharts import options as opts
 from pyecharts.charts import Map
-from pylab import mpl
-import matplotlib.pyplot as plt
+from pyecharts.charts import Bar
 import csv
 
 provinceList = [
     '北京', '广东', '山东', '江苏', '河南', '上海', '河北', '浙江', '香港特别行政区', '陕西', '湖南', '重庆', '福建', '天津', '云南', '四川', '广西壮族自治区',
     '安徽', '海南', '江西', '湖北', '山西', '辽宁', '台湾', '黑龙江', '内蒙古自治区', '澳门特别行政区', '贵州', '甘肃', '青海', '新疆维吾尔自治区', '西藏自治区', '吉林',
     '宁夏回族自治区']
+
 
 def map_visualmap(data) -> Map:
     c = (
@@ -31,19 +31,31 @@ def map_visualmap(data) -> Map:
     return c
 
 
+def bar_base(data) -> Bar:
+    c = (
+        Bar()
+            .add_xaxis(['好评指数', '差评指数'])
+            .add_yaxis("雷克萨斯ES", data)
+            .set_global_opts(title_opts=opts.TitleOpts(title="雷克萨斯ES情感分析", subtitle="好差评指数"))
+    )
+    return c
+
+
 def main():
     userLocList = list()
     locList = list()
     scoreList = list()
-    yscoreList = list()
+    emotionalList = list()
     csvData = csv.reader(open('post.csv', encoding='utf_8_sig'))
     for row in csvData:
         userLoc = row[0] + "|" + row[5][:2]
         if row[8] != 'None':
             score = round(float(row[8]), 5)
             scoreList.append(score)
-            yscoreList.append(1-score)
-            print(score)
+            if score >= 0.5:
+                emotionalList.append(1)
+            else:
+                emotionalList.append(0)
         userLocList.append(userLoc)
     # 根据用户名列表去重
     userLocList = list(set(userLocList))
@@ -59,18 +71,12 @@ def main():
         locDict[i] = locList.count(i)
         locFreq = (i, locList.count(i))
         locFreqList.append(locFreq)
-    # print(locDict)
     print(locFreqList)
+    # 绘制分布图
     c = map_visualmap(locFreqList)
-    # 绘制散点图
-    mpl.rcParams['font.sans-serif'] = ['FangSong']
-    plt.title('汽车口碑分析模型')
-    plt.xlabel('好评指数')
-    plt.ylabel('差评指数')
-    # 数据导入
-    plt.scatter(scoreList, yscoreList, color='r', marker='s')
-    plt.legend()
-    plt.show()
+    # 绘制直方图
+    c2 = bar_base([emotionalList.count(1), emotionalList.count(0)])
+    c2.render()
 
 
 if __name__ == '__main__':
