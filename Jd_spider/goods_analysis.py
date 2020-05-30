f10d3b3a5f2ac7d09431ef3e68b084364ab36899
Peter <quanquanting@qq.com>
2020-05-30 23:36:10 +0800

    添加商品分析

---
+++ Jd_spider/goods_analysis.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python  
+# _*_ coding:utf-8 _*_  
+#  
+# @Version : 1.0  
+# @Time    : 2020/5/30
+# @Author  : 圈圈烃
+# @File    : goods_analysis
+# @Description:
+#
+#
+import pandas as pd
+
+goods = pd.DataFrame(pd.read_csv("jd_goods.csv", header=0, encoding="utf-8-sig"))
+target_goods = goods[goods["评价人数"] > 1000]
+print(target_goods)
